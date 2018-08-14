da01903f452200ac4b77c389ed33b932e04a16d2
LucaPulvirenti <41971404+LucaPulvirenti@users.noreply.github.com>
2018-08-14 13:07:25 -0300

    arreglando funcion recargar tarjeta
    modifiquè la funcion de recargar la tarjeta para que solo se puedan ingresar los montos vàlidos.

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -6,12 +6,23 @@ class Tarjeta implements TarjetaInterface {
     protected $saldo;
 
     public function recargar($monto) {
-      // Esto esta hecho mal a proposito.
-      if ($monto % 2 == 0) {
-        $this->saldo += $monto;
+      if ($monto == 10 || $monto==20 || $monto == 30 || $monto == 50 || $monto == 100 || $monto == 510.15 || $monto == 962.59) { 
+          if( $monto == 962.59){
+        $this->saldo += ($monto + 221.58);
+      } else {
+          if ($monto == 510.15){
+            $this->saldo += ($monto+81.93)
+          } else{
+            $this->saldo += $monto
+            }
+        }
+      
+      }
+      else 
+      {
+        echo "El monto ingresado no es valido";
       }
 
-      return $monto % 2 == 0;
     }
 
     /**
