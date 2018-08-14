7c7fbc34bcc37b13c4f99a218603bea0ffff8943
LucaPulvirenti <41971404+LucaPulvirenti@users.noreply.github.com>
2018-08-14 14:12:25 -0300

    generacion de testeos

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -7,6 +7,12 @@ use PHPUnit\Framework\TestCase;
 class ColectivoTest extends TestCase {
 
     public function testAlgoUtil() {
+    	$coletivo = new Colectivo("144 n","mixta",20); 
+ 
+     $this->assertEquals($coletivo->linea(),"144 n"); 
+     $this->assertEquals($coletivo->empresa(),"mixta"); 
+     $this->assertEquals($coletivo->numero(),20);
+
 
     }
 }
--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -12,11 +12,27 @@ class TarjetaTest extends TestCase {
     public function testCargaSaldo() {
         $tarjeta = new Tarjeta;
 
-        $this->assertTrue($tarjeta->recargar(10));
+        $tarjeta->recargar(10);
         $this->assertEquals($tarjeta->obtenerSaldo(), 10);
 
-        $this->assertTrue($tarjeta->recargar(20));
+        $tarjeta->recargar(20);
         $this->assertEquals($tarjeta->obtenerSaldo(), 30);
+        
+        $tarjeta->recargar(30); 
+        $this->assertEquals($tarjeta->obtenerSaldo(), 60); 
+        
+        $tarjeta->recargar(50); 
+        $this->assertEquals($tarjeta->obtenerSaldo(),110);
+
+        $tarjeta->recargar(100); 
+        $this->assertEquals($tarjeta->obtenerSaldo(),210);
+
+        $tarjeta->recargar(510.15); 
+        $this->assertEquals($tarjeta->obtenerSaldo(),702.08); 
+
+        $tarjeta->recargar(962.59);  
+        $this->assertEquals($tarjeta->obtenerSaldo(),1886.25);
+
     }
 
     /**
@@ -25,7 +41,7 @@ class TarjetaTest extends TestCase {
     public function testCargaSaldoInvalido() {
       $tarjeta = new Tarjeta;
 
-      $this->assertFalse($tarjeta->recargar(15));
+      $tarjeta->recargar(15);
       $this->assertEquals($tarjeta->obtenerSaldo(), 0);
   }
 }
