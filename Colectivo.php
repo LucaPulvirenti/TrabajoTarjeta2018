ee3692dde73b4384d75bb80ec54245cbe78a18ff
LucaPulvirenti <41971404+LucaPulvirenti@users.noreply.github.com>
2018-09-11 18:00:12 -0300

    agregue la funcion pagar

---
+++ Colectivo.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace TrabajoTarjeta;
+
+class Colectivo implements ColectivoInterface {
+
+    protected $linea; 
+    protected $empresa;
+    protected $numero;
+
+    public function __construct($l,$e,$n){
+        $this->linea=$l;
+        $this->empresa=$e; 
+        $this->numero=$n;
+
+    }
+
+    public function linea(){
+        return $this->linea;
+    }
+
+    public function empresa(){
+        return $this->empresa;
+    }
+
+    public function numero(){
+        return $this->numero;
+    } 
+  
+        
+    public function pagarCon(TarjetaInterface $tarjeta){
+          if($tarjeta->pagar==TRUE){
+              if($tarjeta->CantidadPlus()!=0){
+                         $boleto = new Boleto("viaje plus",$this,$tarjeta);
+                          return $boleto;
+                } 
+            else {
+                 $boleto = new Boleto($tarjeta->monto,$this,$tarjeta);
+                          return $boleto;
+                 }
+              
+         } 
+
+             else
+              {
+                  return FALSE;
+                  
+               }
+    }
+
+}
