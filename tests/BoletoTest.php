a25f005e6eeacf26cbb99ee669129af7f08ab84d
pilarkolodziej <pilukolodziej@gmail.com>
2018-09-15 17:57:48 -0300

    correccion boletotest.php

--- tests/BoletoTest.php
+++ tests/BoletoTest.php
@@ -8,7 +8,9 @@ class BoletoTest extends TestCase {
 
     public function testSaldoCero() {
         $valor = 14.80;
-        $boleto = new Boleto($valor, NULL, NULL);
+        $colectivo = new Colectivo("144 r","mixta",712);
+        $tarjeta = new Tarjeta();
+        $boleto = new Boleto($valor,$colectivo,$tarjeta);
 
         $this->assertEquals($boleto->obtenerValor(), $valor);
     }
