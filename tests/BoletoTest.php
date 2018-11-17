c7579d650fe58f32941789ee3947517bc39f350d
LucaPulvirenti <41971404+LucaPulvirenti@users.noreply.github.com>
2018-11-17 16:04:27 -0300

    Add files via upload

--- tests/BoletoTest.php
+++ tests/BoletoTest.php
@@ -5,7 +5,8 @@ namespace TrabajoTarjeta;
 use PHPUnit\Framework\TestCase;
 
 class BoletoTest extends TestCase {
-	$tiempo = new TiempoFalso(0);
+	
+	$tiempo = new TiempoFalso(10);
 	$tarjeta= new Tarjeta($tiempo); 
 	$tarjeta->recargar(100);                            //creamos una tarjeta le cargamos 100pesos
 	$colectivo = new Colecivo("144","semtur",30); 	//creamos un colectivo	
