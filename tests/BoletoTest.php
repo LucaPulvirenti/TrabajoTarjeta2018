82251ef7bc1ef85744566174e4511d74896c7098
LucaPulvirenti <41971404+LucaPulvirenti@users.noreply.github.com>
2018-11-17 15:57:55 -0300

    nuevo testeo boleto

--- tests/BoletoTest.php
+++ tests/BoletoTest.php
@@ -5,6 +5,13 @@ namespace TrabajoTarjeta;
 use PHPUnit\Framework\TestCase;
 
 class BoletoTest extends TestCase {
+	$tiempo = new TiempoFalso(0);
+	$tarjeta= new Tarjeta($tiempo); 
+	$tarjeta->recargar(100);                            //creamos una tarjeta le cargamos 100pesos
+	$colectivo = new Colecivo("144","semtur",30); 	//creamos un colectivo	
+	$boleto = $colectivo->pagarCon($tarjeta);        //pagamos un viaje con la tarjeta y lo almacenamos en la variable boleto.
+
+	$this->assertEquals(get_class($boleto),"TrabajoTarjeta\Boleto"); //verificamos que nos hayan devuelto un boleto
 
    
 }
