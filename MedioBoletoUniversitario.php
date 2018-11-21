5377925350adae3778ca9166aab118f1db308d0b
LucaPulvirenti <41971404+LucaPulvirenti@users.noreply.github.com>
2018-11-21 09:11:56 -0300

    Add files via upload

---
+++ MedioBoletoUniversitario.php
@@ -0,0 +1,72 @@
+<?php 		
+
+namespace TrabajoTarjeta;
+
+class MedioBoletoUniversitario extends Tarjeta Implements TarjetaInterface{
+
+	protected $CantidadBoletos=0; 
+	public $universitario= TRUE;
+	public $monto= 7.4;
+
+	public function PagoUniversitario (){
+
+		if($this->Horas()==FALSE){ 
+			$this->ReiniciarBoleto();
+			$this->CambioMonto();
+			$this->IncrementarBoleto();
+			return $this->pagar();	
+		} 
+		else {
+			if($this->ViajesRestantes()==TRUE) $this->IncrementarBoleto(); 
+			$this->CambioMonto();
+			return $this->pagar();
+		}
+		
+	}
+		
+
+
+    public function CambioMonto(){
+
+	if($this->ViajesRestantes()== TRUE){ 
+	$this->monto=7.4;
+	return $this->monto;
+	}
+	$this->monto= 14.8; 
+	return $this->monto;
+	}
+
+	public function IncrementarBoleto(){
+
+		$this->CantidadBoletos +=1;
+		return $this->CantidadBoletos;
+	}
+
+	public function ReiniciarBoleto(){
+
+			$this->CantidadBoletos==0;
+			
+		
+	}
+
+	public function ViajesRestantes(){
+		if($this->CantidadBoletos<2) return TRUE; 
+		else return FALSE;
+	}
+
+
+	public function Horas(){
+		
+		if($this->DevolverUltimoTiempo() != NULL){ 
+ 		
+		
+		if((int)date('d-m-Y',$this->tiempo->time()) == date('d-m-Y')){
+				return TRUE;									
+
+			}
+		}
+		return FALSE;  //Horas devuelve falso cuando la tarjeta realizará su primer pago, o cuando haya pasado mas de 24 horas con respecto al ultimo pago
+
+	}
+
+}
