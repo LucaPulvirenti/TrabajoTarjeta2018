855135e3b52ce3dfc08b72b7928d308673455a69
LucaPulvirenti <41971404+LucaPulvirenti@users.noreply.github.com>
2018-11-21 15:29:37 -0300

    Add files via upload

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -153,10 +153,9 @@ class TarjetaTest extends TestCase {
 	
     $tiempo7->Avanzar(120); //avanzamos el tiempo 2 minutos
 	$this->assertEquals($tiempo7->time(),120); //verificamos que el tiempo se sume correctamente
-	$this->assertEquals(($tiempo7->time()-$tarjeta->DevolverUltimoTiempo()),120); //verificamos que hayan pasado 120 segundos desde el ultimo pago
-  $this->assertEquals($tarjeta->DevolverUltimoTiempo(),NULL);
-  $this->assertTrue($tarjeta->llega);
-    $this->assertTrue($tarjeta->PagoUniversitario()); //intentamos pagar otro viaje. como pasaron menos de 5 minutos el resultado de pagar deberia ser false
+  
+ 
+    $this->assertFalse($tarjeta->PagoUniversitario()); //intentamos pagar otro viaje. como pasaron menos de 5 minutos el resultado de pagar deberia ser false
 
   }
 
