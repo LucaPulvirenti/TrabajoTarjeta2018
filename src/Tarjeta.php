274a4b4e45807beaad9d69fae6ef9a6851d0900e
LucaPulvirenti <41971404+LucaPulvirenti@users.noreply.github.com>
2018-11-22 08:05:54 -0300

    Add files via upload

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -113,10 +113,11 @@ class Tarjeta implements TarjetaInterface {
 
     if($this->DevolverUltimoTiempo() != NULL) {
      
-        $this->llega=TRUE;
+        
       if($this->tipotarjeta() == 'media franquicia estudiantil' || $this->tipotarjeta()== 'medio universitario') {   
      
-//vardump ($this->tiempo->time())
+//vardump ($this->tiempo->time()) 
+$this->llega=TRUE;
         if($this->tiempo->reciente() - $this->DevolverUltimoTiempo() > 5*60)
         {   
                 if ($this->saldoSuficiente()) 
