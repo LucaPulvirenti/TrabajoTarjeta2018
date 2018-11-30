0be08dd3a14b536c9a125fbd6ca3826ae8549b56
lucapulvirenti <luki.pulvirenti@gmail.com>
2018-11-30 11:04:02 -0300

    agregamos los nuevos metodos de tiempoInterface a la clase tiempo

--- src/Tiempo.php
+++ src/Tiempo.php
@@ -7,5 +7,25 @@ class Tiempo implements TiempoInterface {
     public function reciente() {
 
     	return time();
+    } 
+
+      public function esFeriado()
+    {
+       return FALSE;
+    }
+
+    public function esDeNoche()
+    {
+        return FALSE;
+    }
+
+    public function esFinDeSemana()
+    {
+        return FALSE;
+    }
+
+    public function esDiaSemana()
+    {
+        return TRUE;
     }
 }
