794d033e674ce0e355e1f6b3474894e81eb89af1
lucapulviretni <luki.pulvirenti@gmail.com>
2018-12-02 20:03:14 -0300

    testeando set TRUE

--- tests/TiempoFalsoTest.php
+++ tests/TiempoFalsoTest.php
@@ -12,8 +12,10 @@ class TiempoTest extends TestCase
         $tarjeta = new Tarjeta($tiempo); 
 
         $this->assertTrue($tiempo->esDiaSemana());//por defecto estamos en un dia de semana
-        $this->assertTrue($tiempo->setTrue($tiempo->estado));//al cambiar el estado a true, todos los transbordos abarcaran un tiempo de noventa minutos
+        $tiempo->setTrue($tiempo->estado);
+        $this->assertTrue($tiempo->estado));//al cambiar el estado a true, todos los transbordos abarcaran un tiempo de noventa minutos
 
+        $this->assertFalse($tiempo->estadoDiaSemana());
         $this->assertFalse($tiempo->esDiaSemana());//verificamos que no estemos en un dia de semana
         $this->assertEquals($tarjeta->tiempoTransbordo(),90); //como es fin de semana el tiempo del transbordo deben ser de 90 minutos
 
